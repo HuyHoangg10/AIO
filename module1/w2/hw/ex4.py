d519c272fc43d990340d86b799f024c62af43da5
HuyHoangg <nhh51204@gmail.com>
2026-09-22 11:48:55 +0700

    done homework week2

---
+++ module1/w2/hw/ex4.py
@@ -0,0 +1,18 @@
+def levenshtein_distance(source:str,target:str) -> int:
+    dp = [[0] * (len(target)+1) for _ in range(len(source)+1)]
+
+    for i in range(len(source)+1):
+        dp[i][0] = i
+
+    for j in range(len(target)+1):
+        dp[0][j] = j
+
+    for i in range(1,len(source)+1):
+        for j in range(1,len(target)+1):
+            cost = 0 if source[i-1] == target[j-1] else 1
+            dp[i][j] = min(
+                dp[i-1][j] + 1,
+                dp[i][j-1] + 1,
+                dp[i-1][j-1] + cost
+            )
+    return dp[-1][-1]
