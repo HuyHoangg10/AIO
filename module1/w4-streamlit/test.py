70d7bd411a66035ed6fe5579b75ced494878760f
HuyHoangg <nhh51204@gmail.com>
2026-09-24 17:25:58 +0700

    learn about streamlit

---
+++ module1/w4-streamlit/test.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("HELLO IT's MY PROJECT")
+
+st.write("#Heading 1")
+st.write("##Heading 2")
+st.write("###Heading 3")
+st.code("""
+    name = "Hoang"
+    print(f"Toi ten la f"name")
+""")
